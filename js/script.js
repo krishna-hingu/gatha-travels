de2ab25ddbb58cefe170545da53cd8027794da5a
Krishna Hingu <hingubuilds@gmail.com>
2026-03-29 21:27:56 +0530

    Updated fleet and service

--- js/script.js
+++ js/script.js
@@ -21,3 +21,31 @@ menuLinks.forEach((link) => {
     menuBtn.textContent = "☰";
   });
 });
+
+//hero section slider
+const slides = document.querySelectorAll(".hero-slide");
+let currentSlide = 0;
+
+setInterval(() => {
+  slides[currentSlide].classList.remove("opacity-100");
+  slides[currentSlide].classList.add("opacity-0");
+
+  currentSlide = (currentSlide + 1) % slides.length;
+
+  slides[currentSlide].classList.remove("opacity-0");
+  slides[currentSlide].classList.add("opacity-100");
+}, 3000);
+
+//about section slider
+const aboutSlides = document.querySelectorAll(".about-slide");
+let aboutIndex = 0;
+
+setInterval(() => {
+  aboutSlides[aboutIndex].classList.remove("opacity-100");
+  aboutSlides[aboutIndex].classList.add("opacity-0");
+
+  aboutIndex = (aboutIndex + 1) % aboutSlides.length;
+
+  aboutSlides[aboutIndex].classList.remove("opacity-0");
+  aboutSlides[aboutIndex].classList.add("opacity-100");
+}, 3500);
